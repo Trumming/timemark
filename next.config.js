9f6099cb27f99fe252d27f6cc68e7eee1cc62b01
chuanmin <xcm0215@gmail.com>
2026-01-10 22:49:37 +0800

    chore: add GitHub Pages static deploy workflow and enable next export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
   },
+  output: 'export',
+  // If your repo is not at the root, set basePath and assetPrefix:
+  // basePath: '/timemark',
+  // assetPrefix: '/timemark/',
 }
 
 module.exports = nextConfig
